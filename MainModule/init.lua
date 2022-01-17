d1c1375c2309e4f5c687adca4047b7b84ab89773
imskyyc <santafejayyt@gmail.com>
2022-01-16 20:22:03 -0800

    add license

--- MainModule/init.lua
+++ MainModule/init.lua
@@ -7,4 +7,12 @@
                   \/_/     \/_/ /_/   \/_____/   \/_/  \/_/   \/_____/     \/_/   \/_/\/_/   \/_____/   \/_____/   \/_____/ 
                                                                                                             
 
+
+                                                        The Package Manager for ROBLOX
+
+                                    Prometheus is not just a Server and Client Framework, it is a package manager
+                                    and expandable ecosystem, for developers to share their creations. Prometheus
+                                    is, and will always be open-source. Please see the LICENSE file for more info.
+
+                      Looking to get started with Prometheus? The documentation is available at https://github.com/imskyyc/Prometheus
 ]]--
